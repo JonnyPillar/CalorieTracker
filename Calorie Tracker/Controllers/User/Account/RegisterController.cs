243a5d81dd6d054855621ca6f0ac6a88f6eda5a0
livelyjp <jonny@jonnypillar.co.uk>
2013-08-02 19:40:57 +0100

    Version 1 of Register Controller Added

--- Calorie Tracker/Controllers/User/Account/RegisterController.cs
+++ Calorie Tracker/Controllers/User/Account/RegisterController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Calorie_Tracker.DAL;
 
 namespace Calorie_Tracker.Controllers.User.Account
 {
     public class RegisterController : Controller
     {
+        private calorie_tracker_v1Entities db = new calorie_tracker_v1Entities();
+
         //
         // GET: /Register/
 
@@ -23,12 +27,25 @@ namespace Calorie_Tracker.Controllers.User.Account
         }
 
         [HttpPost]
-        public ActionResult Register(Models.User user)
+        public ActionResult Register(DAL.tbl_user user)
         {
             if (ModelState.IsValid)
             {
-
+                if (user.IsValid(user.user_password))
+                {
+                    if (string.IsNullOrEmpty(user.user_id)) user.user_id = Guid.NewGuid().ToString();
+                    user.user_password_hash = string.Empty;
+                    db.tbl_user.Add(user);
+                    db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(user.user_email, true); //TODO Does it need to be true?
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login data is incorrect!");
+                }
             }
+            return View(user);
         }
     }
 }
--- Calorie Tracker/DAL/tbl_user.cs
+++ Calorie Tracker/DAL/tbl_user.cs
@@ -27,17 +27,21 @@ namespace Calorie_Tracker.DAL
 
         [HiddenInput(DisplayValue = false)]
         public string user_id { get; set; }
+
         [Display(Name = "Email Address")]
         [Required(ErrorMessage = "Please Provide A Valid Email Address")]
         [DataType(DataType.EmailAddress)]
         [EmailValidator]
         public string user_email { get; set; }
+
         [Display(Name = "First Name")]
         [Required(ErrorMessage = "Please Provide A Valid Name")]
         public string user_first_name { get; set; }
+
         [Display(Name = "Second Name")]
         [Required(ErrorMessage = "Please Provide A Valid Name")]
         public string user_second_name { get; set; }
+
         [Display(Name = "Date Of Birth")]
         [Required(ErrorMessage = "Please Provide A Valid Date Of Birth")]
         //[DateOfBirthValidator]
@@ -45,17 +49,16 @@ namespace Calorie_Tracker.DAL
 
         public string user_password { get; set; }
         public string user_passwordConfim { get; set; }
-
         public string user_password_salt { get; set; }
+        public string user_password_hash { get; set; }
+
         [Display(Name = "Created Date")]
         public string user_creation_date { get; set; }
-        public string user_password_hash { get; set; }
-    
+        
         public virtual ICollection<tbl_food_log> tbl_food_log { get; set; }
         public virtual ICollection<tbl_user_information> tbl_user_information { get; set; }
         public virtual ICollection<tbl_user_target> tbl_user_target { get; set; }
 
-        //Valid for Logon
         /// <summary>
         /// Is User Object Valid For Logon
         /// </summary>
@@ -66,18 +69,17 @@ namespace Calorie_Tracker.DAL
         {
             DataConnection connection = new DataConnection("SELECT * FROM tbl_user WHERE user_email = ?", typeof(tbl_user));
             connection.AddParameter(_email);
+            tbl_user[] dbItem = (tbl_user[])connection.ExecuteQuery();
             //Get User Object Back
-            object userObject = new object();
-            if (userObject != null)
+            if (dbItem.Length == 1)
             {
                 string dbSalt = string.Empty;
                 string dbHash = string.Empty;
-                if (PasswordHasher.IsPasswordValid(dbHash, dbSalt, _password)) return true; //Is Inputed Pass Same As Ours
+                if (PasswordHasher.IsPasswordValid(dbItem[0].user_password_hash, dbItem[0].user_password_salt, _password)) return true; //Is Inputed Pass Same As Ours
             }
             return false;
         }
 
-        //Is It Valid for Signup
         /// <summary>
         /// Is the User Object Valid For Signup
         /// </summary>
