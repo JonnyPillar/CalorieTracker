388d89bce040e262320df30b9659ebeb70f66996
livelyjp <jonny@jonnypillar.co.uk>
2013-08-06 13:28:36 +0100

    Start Work On User Dashboard

---
+++ Calorie Tracker/Controllers/User/Dashboard/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calorie_Tracker.DAL;
+
+namespace Calorie_Tracker.Controllers.User.Dashboard
+{
+    public class DashboardController : Controller
+    {
+        private calorie_tracker_v1Entities db = new calorie_tracker_v1Entities();
+        //
+        // GET: /Dashboard/
+
+        public ActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated) return RedirectToAction("Index", "Home");
+            tbl_user user = db.tbl_user.First(tbl_user => tbl_user.user_email == User.Identity.Name);
+            return View(user);
+        }
+    }
+}
---
+++ Calorie Tracker/Models/Dashboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Calorie_Tracker.DAL;
+
+namespace Calorie_Tracker.Models
+{
+    public class Dashboard
+    {
+        private calorie_tracker_v1Entities db = new calorie_tracker_v1Entities();
+
+        public Dashboard()
+        {
+
+        }
+
+        public Dashboard(string userID)
+        {
+            this.user = db.tbl_user.Find(userID);
+        }
+
+        public tbl_user user { get; set; }
+    }
+}
