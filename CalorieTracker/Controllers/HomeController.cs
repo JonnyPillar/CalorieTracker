cb6f69d97bc9e6487462aeccc31b18605bb0ab61
livelyjp <jonny@jonnypillar.co.uk>
2014-02-04 15:56:46 +0000

    Modifications to MVC5 update before DB update

--- CalorieTracker/Controllers/HomeController.cs
+++ CalorieTracker/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CalorieTracker.Models;
+using WebGrease;
 
 namespace CalorieTracker.Controllers
 {
     public class HomeController : Controller
     {
+        //private calo
+
+
         public ActionResult Index()
         {
+            tbl_user temp = new tbl_user();
+            temp.user_gender = 0;
+            temp.user_creation_timestamp = DateTime.UtcNow;
+            temp.user_admin = 0;
+            temp.user_password_hash = "";
+            temp.user_password_salt = "";
+
+            //temp.
+
             return View();
+            
         }
 
         public ActionResult About()
