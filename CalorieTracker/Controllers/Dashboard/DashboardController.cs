30263583706a500c9e650d22e15105f64a61fb23
livelyjp <jonny@jonnypillar.co.uk>
2014-02-06 18:48:25 +0000

    Dashboard and Review Controllers and Views setup

--- CalorieTracker/Controllers/Dashboard/DashboardController.cs
+++ CalorieTracker/Controllers/Dashboard/DashboardController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CalorieTracker.Controllers.Dashboard
 {
@@ -10,88 +6,15 @@ namespace CalorieTracker.Controllers.Dashboard
     {
         //
         // GET: /Dashboard/
+
+        /// <summary>
+        ///     View Users Info
+        /// </summary>
+        /// <returns>Dashboard View</returns>
         public ActionResult Index()
         {
+            //Check Logged In
             return View();
-        }
-
-        //
-        // GET: /Dashboard/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /Dashboard/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Dashboard/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Dashboard/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Dashboard/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Dashboard/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Dashboard/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
---
+++ CalorieTracker/Controllers/Dashboard/ReviewController.cs
@@ -0,0 +1,75 @@
+﻿using System.Web.Mvc;
+
+namespace CalorieTracker.Controllers.Dashboard
+{
+    public class ReviewController : Controller
+    {
+        //
+        // GET: /Review/
+        /// <summary>
+        ///     Latest User Review Day
+        /// </summary>
+        /// <returns>Review Choice View</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Day Review
+        /// </summary>
+        /// <returns>Day Review View</returns>
+        public ActionResult Day()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Dat Review
+        /// </summary>
+        /// <param name="dayNumber">Day Number of Year</param>
+        /// <returns>Day Review View</returns>
+        public ActionResult Day(int dayNumber)
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Weej Review
+        /// </summary>
+        /// <returns>Week Review View</returns>
+        public ActionResult Week()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Week Review
+        /// </summary>
+        /// <param name="weekNumber">Week Number</param>
+        /// <returns>Week Review View</returns>
+        public ActionResult Week(int weekNumber)
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Year Review
+        /// </summary>
+        /// <returns>Year Review View</returns>
+        public ActionResult Year()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Year Review
+        /// </summary>
+        /// <param name="yearNumber">Year</param>
+        /// <returns>Year Review View</returns>
+        public ActionResult Year(int yearNumber)
+        {
+            return View();
+        }
+    }
+}
