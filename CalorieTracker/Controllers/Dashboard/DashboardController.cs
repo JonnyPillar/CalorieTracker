f5dc54140e80d93b0cfb0dda1c24a30a7cd5f2a0
livelyjp <jonny@jonnypillar.co.uk>
2014-02-10 20:28:35 +0000

    Updated Dashboard to allow for new user pass in

--- CalorieTracker/Controllers/Dashboard/DashboardController.cs
+++ CalorieTracker/Controllers/Dashboard/DashboardController.cs
@@ -1,23 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using CalorieTracker.Models;
+using CalorieTracker.Models.ViewModels;
+using CalorieTracker.Utils.Account;
+using Microsoft.Ajax.Utilities;
 
 namespace CalorieTracker.Controllers.Dashboard
 {
     public class DashboardController : Controller
     {
+        private readonly CTDBContainer db = new CTDBContainer();
         //
         // GET: /Dashboard/
-
-        /// <summary>
-        ///     View Users Info
-        /// </summary>
-        /// <returns>Dashboard View</returns>
-        [HttpGet]
-        public ActionResult Index()
-        {
-            //Check Logged In
-            if (!User.Identity.IsAuthenticated) return RedirectToAction("Index", "Login");
-            return View();
-        }
 
         /// <summary>
         ///     View Users Info
@@ -28,7 +23,16 @@ namespace CalorieTracker.Controllers.Dashboard
         {
             //Check Logged In
             if (!User.Identity.IsAuthenticated) return RedirectToAction("Index", "Login");
-            return View();
+            int userID = IdentityUtil.GetUserIDFromCookie(User);
+            User dashboardUser = db.Users.FirstOrDefault(user => user.UserID == userID);
+
+            if (dashboardUser == null)
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Index", "Home");
+            }
+            var dashboardModel = new DashboardModel(id.IsNullOrWhiteSpace(), dashboardUser);
+            return View(dashboardModel);
         }
     }
 }
